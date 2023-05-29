8e6d80eb3b4303256cbcbe04960242f6f412a896
Natalia <NSzymczak>
2023-05-29 19:25:07 +0200

    try to fix

--- src/main/java/pl/opole/uni/springWebApp/config/SecurityConfig.java
+++ src/main/java/pl/opole/uni/springWebApp/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package pl.opole.uni.springWebApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.opole.uni.springWebApp.controllers.AcountController;
+import pl.opole.uni.springWebApp.models.UserAcount;
 import pl.opole.uni.springWebApp.services.UserAcountService;
 
 @Configuration
@@ -30,16 +33,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity httpSecurity) throws Exception
   {
     httpSecurity
-      //.httpBasic()
-      //.and()
-      //.csrf().disable()
+      .httpBasic()
+      .and()
+      .csrf().disable()
       .authorizeRequests()
-      .antMatchers(HttpMethod.POST, "/students").hasRole("MODERATOR")
-      .antMatchers("/subjects").permitAll();
-    //.and()
-    //.formLogin().permitAll()
-    //.and()
-    //.logout();
+      .antMatchers(HttpMethod.GET,"/Product").permitAll();
   }
 
   @Bean
@@ -48,6 +46,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     return new BCryptPasswordEncoder();
   }
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void establishUsers()
+	{
+    //userAcountService.saveUser(new UserAcount("michal", passwordEncoder().encode("michal"), "ROLE_USER"));
+    //userAcountService.saveUser(new UserAcount("admin", passwordEncoder().encode("admin"), "ROLE_ADMIN"));
+    //userAcountService.saveUser(new UserAcount("jan", passwordEncoder().encode("jan"), "ROLE_MODERATOR"));
+    //userAcountService.saveUser(new UserAcount("kuba", passwordEncoder().encode("kuba"), "ROLE_USER"));
+	}
 
 //  @Override
 //  protected void configure(HttpSecurity http) throws Exception {
